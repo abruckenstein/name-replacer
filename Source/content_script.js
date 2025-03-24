87bfec733a858dd958a53903c2e520000ad6bbf5
abruckenstein <alanjbruckenstein@gmail.com>
2025-03-24 14:41:19 -0400

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,8 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bChase\b/g, "Ellie");
+	v = v.replace(/\bchase\b/g, "ellie");
 	
 	textNode.nodeValue = v;
 }
